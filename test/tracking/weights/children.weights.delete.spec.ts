c8c2b5a3e095528f3d424eee09eafb39938e4bd7
Thairam <thayram01@gmail.com>
2019-11-05 12:32:01 -0300

    create tests of weight.delete

---
+++ test/tracking/weights/children.weights.delete.spec.ts
@@ -0,0 +1,300 @@
+import request from 'supertest'
+import { expect } from 'chai'
+import { acc } from '../../utils/account.utils'
+import { accountDB } from '../../../src/account-service/database/account.db'
+import { trck } from '../../utils/tracking.utils'
+import { trackingDB } from '../../../src/tracking-service/database/tracking.db'
+import { ApiGatewayException } from '../../utils/api.gateway.exceptions'
+import { Institution } from '../../../src/account-service/model/institution'
+import { Child } from '../../../src/account-service/model/child'
+import { ChildMock } from '../../mocks/account-service/child.mock'
+import { Educator } from '../../../src/account-service/model/educator'
+import { EducatorMock } from '../../mocks/account-service/educator.mock'
+import { HealthProfessional } from '../../../src/account-service/model/health.professional'
+import { HealthProfessionalMock } from '../../mocks/account-service/healthprofessional.mock'
+import { Family } from '../../../src/account-service/model/family'
+import { FamilyMock } from '../../mocks/account-service/family.mock'
+import { Application } from '../../../src/account-service/model/application'
+import { ApplicationMock } from '../../mocks/account-service/application.mock'
+import { Weight } from '../../../src/tracking-service/model/weight'
+import { WeightMock } from '../../mocks/tracking-service/weight.mock'
+
+describe('Routes: children.weights', () => {
+
+    const URI: string = process.env.AG_URL || 'https://localhost:8081'
+
+    let accessTokenAdmin: string
+    let accessDefaultChildToken: string
+    let accessDefaultEducatorToken: string
+    let accessDefaultHealthProfessionalToken: string
+    let accessDefaultFamilyToken: string
+    let accessDefaultApplicationToken: string
+
+    const defaultInstitution: Institution = new Institution()
+    defaultInstitution.type = 'default type'
+    defaultInstitution.name = 'default name'
+    defaultInstitution.address = 'default address'
+    defaultInstitution.latitude = 0
+    defaultInstitution.longitude = 0
+
+    const defaultChild: Child = new ChildMock()
+    const defaultEducator: Educator = new EducatorMock()
+    const defaultHealthProfessional: HealthProfessional = new HealthProfessionalMock()
+    const defaultFamily: Family = new FamilyMock()
+    const defaultApplication: Application = new ApplicationMock()
+
+    const defaultWeight: Weight = new WeightMock()
+
+    before(async () => {
+        try {
+            await accountDB.connect()
+            await trackingDB.connect()
+
+            const tokens = await acc.getAuths()
+            accessTokenAdmin = tokens.admin.access_token
+
+            const resultDefaultInstitution = await acc.saveInstitution(accessTokenAdmin, defaultInstitution)
+            defaultInstitution.id = resultDefaultInstitution.id
+            defaultChild.institution = resultDefaultInstitution
+            defaultEducator.institution = resultDefaultInstitution
+            defaultHealthProfessional.institution = resultDefaultInstitution
+            defaultFamily.institution = resultDefaultInstitution
+            defaultApplication.institution = resultDefaultInstitution
+
+            const resultDefaultChild = await acc.saveChild(accessTokenAdmin, defaultChild)
+            defaultChild.id = resultDefaultChild.id
+            defaultFamily.children = new Array<Child>(resultDefaultChild)
+
+            const resultDefaultEducator = await acc.saveEducator(accessTokenAdmin, defaultEducator)
+            defaultEducator.id = resultDefaultEducator.id
+
+            const resultDefaultHealthProfessional = await acc.saveHealthProfessional(accessTokenAdmin, defaultHealthProfessional)
+            defaultHealthProfessional.id = resultDefaultHealthProfessional.id
+
+            const resultDefaultFamily = await acc.saveFamily(accessTokenAdmin, defaultFamily)
+            defaultFamily.id = resultDefaultFamily.id
+
+            const resultDefaultApplication = await acc.saveApplication(accessTokenAdmin, defaultApplication)
+            defaultApplication.id = resultDefaultApplication.id
+
+            //deleteting tokens for each 'default user'
+            if (defaultChild.username && defaultChild.password) {
+                accessDefaultChildToken = await acc.auth(defaultChild.username, defaultChild.password)
+            }
+
+            if (defaultEducator.username && defaultEducator.password) {
+                accessDefaultEducatorToken = await acc.auth(defaultEducator.username, defaultEducator.password)
+            }
+
+            if (defaultFamily.username && defaultFamily.password) {
+                accessDefaultFamilyToken = await acc.auth(defaultFamily.username, defaultFamily.password)
+            }
+
+            if (defaultApplication.username && defaultApplication.password) {
+                accessDefaultApplicationToken = await acc.auth(defaultApplication.username, defaultApplication.password)
+            }
+
+            if (defaultHealthProfessional.username && defaultHealthProfessional.password) {
+                accessDefaultHealthProfessionalToken = await acc.auth(defaultHealthProfessional.username, defaultHealthProfessional.password)
+            }
+
+        } catch (err) {
+            console.log('Failure on Before from weight.delete test: ', err.message)
+        }
+    })
+    after(async () => {
+        try {
+            await accountDB.removeCollections()
+            await trackingDB.removeCollections()
+            await accountDB.dispose()
+            await trackingDB.dispose()
+        } catch (err) {
+            console.log('DB ERROR', err)
+        }
+    })
+
+    describe('DELETE /children/:child_id/weights/:weight_id', () => {
+
+        beforeEach(async () => {
+            try {
+                const resultWeight = await trck.saveWeight(accessDefaultChildToken, defaultWeight, defaultChild.id)
+                defaultWeight.id = resultWeight.id
+                defaultWeight.child_id = defaultChild.id
+            } catch (err) {
+                console.log('Failure in weight.delete test: ', err.message)
+            }
+        })
+        afterEach(async () => {
+            try {
+                await trackingDB.deleteMeasurements()
+            } catch (err) {
+                console.log('Failure in weight.delete test: ', err.message)
+            }
+        })
+
+        context('when the user delete the weight of the child successfully', () => {
+
+            it('weight.delete001: should return status code 204 and no content for weight, for educator user', () => {
+
+                return request(URI)
+                    .delete(`/children/${defaultChild.id}/weights/${defaultWeight.id}`)
+                    .set('Content-Type', 'application/json')
+                    .set('Authorization', 'Bearer '.concat(accessDefaultEducatorToken))
+                    .expect(204)
+                    .then(res => {
+                        expect(res.body).to.eql({})
+                    })
+            })
+
+            it('weight.delete002: should return status code 204 and no content for weight, for family user', () => {
+
+                return request(URI)
+                    .delete(`/children/${defaultChild.id}/weights/${defaultWeight.id}`)
+                    .set('Content-Type', 'application/json')
+                    .set('Authorization', 'Bearer '.concat(accessDefaultFamilyToken))
+                    .expect(204)
+                    .then(res => {
+                        expect(res.body).to.eql({})
+                    })
+            })
+
+            it('weight.delete003: should return status code 204 and no content for weight, for application user', () => {
+
+                return request(URI)
+                    .delete(`/children/${defaultChild.id}/weights/${defaultWeight.id}`)
+                    .set('Content-Type', 'application/json')
+                    .set('Authorization', 'Bearer '.concat(accessDefaultApplicationToken))
+                    .expect(204)
+                    .then(res => {
+                        expect(res.body).to.eql({})
+                    })
+            })
+
+        }) //user delete all weights of a child successfully
+
+        context('when a validation error occurs', () => {
+            const INVALID_ID = '123'
+
+            it('weight.delete004: should return status code 400 and info message from child_id is invalid', () => {
+
+
+                return request(URI)
+                    .delete(`/children/${INVALID_ID}/weights/${defaultWeight.id}`)
+                    .set('Content-Type', 'application/json')
+                    .set('Authorization', 'Bearer '.concat(accessDefaultEducatorToken))
+                    .expect(400)
+                    .then(err => {
+                        expect(err.body).to.eql(ApiGatewayException.WEIGHTS.ERROR_400_INVALID_CHILD_ID)
+                    })
+            })
+
+            it('weight.delete005: should return status code 400 and info message from weight_id is invalid', () => {
+
+                return request(URI)
+                    .delete(`/children/${defaultChild.id}/weights/${INVALID_ID}`)
+                    .set('Content-Type', 'application/json')
+                    .set('Authorization', 'Bearer '.concat(accessDefaultEducatorToken))
+                    .expect(400)
+                    .then(err => {
+                        expect(err.body).to.eql(ApiGatewayException.WEIGHTS.ERROR_400_INVALID_WEIGHT_ID)
+                    })
+            })
+        }) // when a validation error occurs
+
+        context('when the weight is not found', () => {
+            const NON_EXISTENT_ID = '111111111111111111111111'
+
+            it('weight.delete006: should return status code 204 and no content for weight, when the child not exist', () => {
+
+                return request(URI)
+                    .delete(`/children/${NON_EXISTENT_ID}/weights/${defaultWeight.id}`)
+                    .set('Content-Type', 'application/json')
+                    .set('Authorization', 'Bearer '.concat(accessDefaultEducatorToken))
+                    .expect(204)
+                    .then(res => {
+                        expect(res.body).to.eql({})
+                    })
+            })
+
+            it('weight.delete007: should return status code 204 and no content for weight, when the weight not exist', () => {
+
+                return request(URI)
+                    .delete(`/children/${defaultChild.id}/weights/${NON_EXISTENT_ID}`)
+                    .set('Content-Type', 'application/json')
+                    .set('Authorization', 'Bearer '.concat(accessDefaultEducatorToken))
+                    .expect(204)
+                    .then(res => {
+                        expect(res.body).to.eql({})
+                    })
+            })
+        }) // when the weight is not found
+
+        context('when the user does not have permission for delete weight', () => {
+            it('weight.delete008: should return status code 403 and info message from insufficient permissions for admin user', () => {
+
+                return request(URI)
+                    .delete(`/children/${defaultChild.id}/weights/${defaultWeight.id}`)
+                    .set('Content-Type', 'application/json')
+                    .set('Authorization', 'Bearer '.concat(accessTokenAdmin))
+                    .expect(403)
+                    .then(err => {
+                        expect(err.body).to.eql(ApiGatewayException.ERROR_MESSAGE.ERROR_403_FORBIDDEN)
+                    })
+            })
+
+            it('weight.delete009: should return status code 403 and info message from insufficient permissions for the child', () => {
+
+                return request(URI)
+                    .delete(`/children/${defaultChild.id}/weights/${defaultWeight.id}`)
+                    .set('Content-Type', 'application/json')
+                    .set('Authorization', 'Bearer '.concat(accessDefaultChildToken))
+                    .expect(403)
+                    .then(err => {
+                        expect(err.body).to.eql(ApiGatewayException.ERROR_MESSAGE.ERROR_403_FORBIDDEN)
+                    })
+            })
+
+            it('weight.delete010: should return status code 403 and info message from insufficient permissions for health professional user', () => {
+
+                return request(URI)
+                    .delete(`/children/${defaultChild.id}/weights/${defaultWeight.id}`)
+                    .set('Content-Type', 'application/json')
+                    .set('Authorization', 'Bearer '.concat(accessDefaultHealthProfessionalToken))
+                    .expect(403)
+                    .then(err => {
+                        expect(err.body).to.eql(ApiGatewayException.ERROR_MESSAGE.ERROR_403_FORBIDDEN)
+                    })
+            })
+        }) // when the user does not have permission for delete weight
+
+        describe('when not informed the acess token', () => {
+            it('weight.delete011: should return the status code 401 and the authentication failure informational message', () => {
+
+                return request(URI)
+                    .delete(`/children/${defaultChild.id}/weights/${defaultWeight.id}`)
+                    .set('Content-Type', 'application/json')
+                    .set('Authorization', 'Bearer ')
+                    .expect(401)
+                    .then(err => {
+                        expect(err.body).to.eql(ApiGatewayException.AUTH.ERROR_401_UNAUTHORIZED)
+                    })
+            })
+        })
+
+        describe('when delete the weight of a child that has been deleted', () => {
+            it('weight.delete012: should return status code 204 and no content for weight, when the child not exist', async () => {
+
+                await acc.deleteUser(accessTokenAdmin, defaultChild.id)
+
+                return request(URI)
+                    .delete(`/children/${defaultChild.id}/weights/${defaultWeight.id}`)
+                    .set('Content-Type', 'application/json')
+                    .set('Authorization', 'Bearer '.concat(accessDefaultEducatorToken))
+                    .expect(204)
+                    .then(err => {
+                        expect(err.body).to.eql({})
+                    })
+            })
+        })
+    })
+})
