f0fa3d5253d37d751b6d2dd95a42908d3ba2f498
Adson-Macedo <agnsoft@hotmail.com>
2020-11-13 10:54:01 -0300

    Fixing defaultChild credentials in account/users.

--- test/account/users/users.post.spec.ts
+++ test/account/users/users.post.spec.ts
@@ -33,6 +33,8 @@ describe('Routes: users', () => {
     defaultInstitution.longitude = 0
 
     const defaultChild: Child = new ChildMock()
+    defaultChild.username = 'default child'
+    defaultChild.password = 'default pass'
 
     const defaultEducator: Educator = new Educator()
     defaultEducator.username = 'default educator'
